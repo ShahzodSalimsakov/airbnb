231879f652750e661c020a3d83ff6c99c71c94c4
Doniman <doniman2409@gmail.com>
2020-06-09 16:16:21 +0500

    Исправил Cancel

--- pages/become-a-host/index.vue
+++ pages/become-a-host/index.vue
@@ -3,21 +3,57 @@
     <template>
       <v-stepper v-model="e1">
         <v-stepper-header>
-          <v-stepper-step :complete="e1 > 1" step="1"
-            >Name of step 1</v-stepper-step
-          >
+          <v-stepper-step :complete="e1 > 1" step="1">
+            Name of step 1
+          </v-stepper-step>
 
           <v-divider></v-divider>
 
-          <v-stepper-step :complete="e1 > 2" step="2"
-            >Name of step 2</v-stepper-step
-          >
+          <v-stepper-step :complete="e1 > 2" step="2">
+            Name of step 2
+          </v-stepper-step>
 
           <v-divider></v-divider>
 
-          <v-stepper-step :complete="e1 > 3" step="3"
-            >Name of step 3</v-stepper-step
-          >
+          <v-stepper-step :complete="e1 > 3" step="3">
+            Name of step 3
+          </v-stepper-step>
+
+          <v-divider></v-divider>
+
+          <v-stepper-step :complete="e1 > 4" step="4">
+            Name of step 4
+          </v-stepper-step>
+
+          <v-divider></v-divider>
+
+          <v-stepper-step :complete="e1 > 5" step="5">
+            Name of step 5
+          </v-stepper-step>
+
+          <v-divider></v-divider>
+
+          <v-stepper-step :complete="e1 > 6" step="6">
+            Name of step 6
+          </v-stepper-step>
+
+          <v-divider></v-divider>
+
+          <v-stepper-step :complete="e1 > 7" step="7">
+            Name of step 7
+          </v-stepper-step>
+
+          <v-divider></v-divider>
+
+          <v-stepper-step :complete="e1 > 8" step="8">
+            Name of step 8
+          </v-stepper-step>
+
+          <v-divider></v-divider>
+
+          <v-stepper-step :complete="e1 > 9" step="9">
+            Name of step 9
+          </v-stepper-step>
 
           <v-divider></v-divider>
 
@@ -45,7 +81,7 @@
               Continue
             </v-btn>
 
-            <v-btn text>Cancel</v-btn>
+            <v-btn text @click="e1 = 1">Cancel</v-btn>
           </v-stepper-content>
 
           <v-stepper-content step="3">
@@ -55,7 +91,7 @@
               Continue
             </v-btn>
 
-            <v-btn text>Cancel</v-btn>
+            <v-btn text @click="e1 = 2">Cancel</v-btn>
           </v-stepper-content>
 
           <v-stepper-content step="4">
@@ -65,7 +101,7 @@
               Continue
             </v-btn>
 
-            <v-btn text>Cancel</v-btn>
+            <v-btn text @click="e1 = 3">Cancel</v-btn>
           </v-stepper-content>
 
           <v-stepper-content step="5">
@@ -75,7 +111,7 @@
               Continue
             </v-btn>
 
-            <v-btn text>Cancel</v-btn>
+            <v-btn text @click="e1 = 4">Cancel</v-btn>
           </v-stepper-content>
 
           <v-stepper-content step="6">
@@ -85,7 +121,7 @@
               Continue
             </v-btn>
 
-            <v-btn text>Cancel</v-btn>
+            <v-btn text @click="e1 = 5">Cancel</v-btn>
           </v-stepper-content>
 
           <v-stepper-content step="7">
@@ -95,7 +131,7 @@
               Continue
             </v-btn>
 
-            <v-btn text>Cancel</v-btn>
+            <v-btn text @click="e1 = 6">Cancel</v-btn>
           </v-stepper-content>
 
           <v-stepper-content step="8">
@@ -103,7 +139,7 @@
               Continue
             </v-btn>
 
-            <v-btn text>Cancel</v-btn>
+            <v-btn text @click="e1 = 7">Cancel</v-btn>
           </v-stepper-content>
         </v-stepper-items>
       </v-stepper>
