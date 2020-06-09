3f8d58437329b78e1f7240f8fa43aaea53599d4d
shahaa <projects.salim95@gmail.com>
2020-06-09 15:43:46 +0500

    step-1
    add radio

--- components/become-a-host/WizardRoom.vue
+++ components/become-a-host/WizardRoom.vue
@@ -3,26 +3,73 @@
     <v-col cols="12" sm="10" md="8" lg="6">
       <v-card ref="form">
         <v-card-text>
-          <v-autocomplete
+          <v-select
             ref="place"
             v-model="place"
-            :rules="[() => !!place || 'This field is required']"
-            :items="place"
+            :items="placeItems"
             label="Для начала сузим выбор"
             placeholder="Выберите 1 вариант"
-            required
-          ></v-autocomplete> </v-card-text
-        ><v-card-text>
-          <v-autocomplete
-            ref="type_of_housing"
+          ></v-select>
+        </v-card-text>
+        <v-card-text>
+          <v-select
+            ref="typeOfHousing"
             v-model="type_of_housing"
-            :items="type_of_housing"
+            :items="typeHouses"
             label="Теперь выберите тип жилья"
             placeholder="Выберите тип жилья"
-            required
-          ></v-autocomplete>
+          ></v-select>
         </v-card-text>
-        <v-divider class="mt-12"></v-divider>
+        <v-card-text>
+          <h3>Что будет в распоряжении гостей?</h3>
+          <v-radio-group v-model="disposalOfGuests" :mandatory="false">
+            <v-radio label="Жилье целиком" value="radio-1"></v-radio>
+            <div>
+              Всё жилье целиком в распоряжении гостей: обычно это спальня,
+              ванная и кухня.
+            </div>
+            <v-radio label="Отдельная комната" value="radio-2"></v-radio>
+            <div>
+              Гостям предоставляется отдельная спальня, остальные помещения
+              используются совместно.
+            </div>
+            <v-radio label="Место в комнате" value="radio-3"></v-radio>
+            <div>
+              Гости спят в общей спальне или помещении с другими людьми.
+            </div>
+          </v-radio-group>
+        </v-card-text>
+        <v-card-text>
+          <h3>Это помещение предназначено только для гостей?</h3>
+          <v-radio-group v-model="forGuest" :mandatory="false">
+            <v-radio
+              label="Да, им пользуются в основном только гости"
+              value="radio-4"
+            ></v-radio>
+            <v-radio
+              label="Нет, я храню здесь личные вещи"
+              value="radio-5"
+            ></v-radio>
+          </v-radio-group>
+        </v-card-text>
+        <v-card-text>
+          <h3>Вы размещаете объявление на Airbnb от лица компании?</h3>
+          <v-radio-group v-model="onBehalfOfTheCompany" :mandatory="false">
+            <v-radio
+              label="Да, я управляю компанией или работаю в ней"
+              value="radio-6"
+            ></v-radio>
+            <v-radio
+              label="Нет, это ко мне не относится"
+              value="radio-7"
+            ></v-radio>
+            <div>
+              Это даст вам доступ к подходящим функциям Airbnb — гости этого не
+              увидят, и выбор никак не повлияет на позиции вашего жилья в
+              поиске.
+            </div>
+          </v-radio-group>
+        </v-card-text>
       </v-card>
     </v-col>
   </v-row>
@@ -31,22 +78,73 @@
 <script>
 export default {
   data: () => ({
-    place: [
-      'Квартира',
-      'Дом',
-      'Жилье при доме / квартире',
-      'Уникальное жилье',
-      'B&B',
-      'Бутик-отель'
+    disposalOfGuests: '',
+    forGuest: '',
+    onBehalfOfTheCompany: '',
+    place: '',
+    typeOfHousing: '',
+    placeItems: [
+      {
+        value: 1,
+        text: 'Квартира'
+      },
+      {
+        value: 2,
+        text: 'Дом'
+      },
+      {
+        value: 3,
+        text: 'Жилье при доме / квартире'
+      },
+      {
+        value: 4,
+        text: 'Уникальное жилье'
+      },
+      {
+        value: 5,
+        text: 'B&B'
+      },
+      {
+        value: 6,
+        text: 'Бутик-отель'
+      }
     ],
     type_of_housing: [
-      'Квартира',
-      'Кондоминиум',
-      'Каса партикуляр (Куба)',
-      'Лофт',
-      'Апартаменты'
+      {
+        value: 1,
+        text: 'Хижина',
+        type: [1, 2]
+      },
+      {
+        value: 2,
+        text: 'Кондоминиум',
+        type: [1, 2]
+      },
+      {
+        value: 3,
+        text: 'Каса партикуляр (Куба)',
+        type: [1]
+      },
+      {
+        value: 4,
+        text: 'Лофт',
+        type: [1]
+      },
+      {
+        value: 5,
+        text: 'Апартаменты',
+        type: [1]
+      }
     ]
-  })
+  }),
+  computed: {
+    typeHouses() {
+      return [...this.type_of_housing].filter((item) =>
+        item.type.includes(this.place)
+      )
+    }
+  },
+  methods: {}
 }
 </script>
 
