09f63abf3c659b3ecfcf58b347536393d6b39878
shahaa <projects.salim95@gmail.com>
2020-06-12 18:47:51 +0500

    separate component for input

---
+++ components/inputs/IncrementDecrement.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="d-flex">
+    <div class="d-inline">
+      <div class="d-inline-block">
+        <v-text-field
+          v-model="val"
+          type="number"
+          min="0"
+          :placeholder="placeholder"
+          @input="checkDisable"
+        />
+      </div>
+      <v-btn :disabled="disabledButton" rounded @click="decrement">-</v-btn>
+      <v-btn rounded @click="increment">+</v-btn>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'IncrementDecrement',
+  props: ['placeholder'],
+  data: () => ({
+    val: '',
+    disabledButton: null
+  }),
+  methods: {
+    increment() {
+      this.val++
+      this.disabledButton = false
+    },
+    decrement() {
+      if (this.val > 0) {
+        this.disabledButton = false
+        this.val--
+      }
+
+      if (this.val === 0) {
+        this.disabledButton = true
+      }
+    },
+    checkDisable() {
+      if (this.val <= 0) {
+        this.disabledButton = true
+      } else {
+        this.disabledButton = false
+      }
+    }
+  }
+}
+</script>
+
+<style scoped></style>
--- components/step-3/WizardHowLongCanGuestsStay.vue
+++ components/step-3/WizardHowLongCanGuestsStay.vue
@@ -6,46 +6,8 @@
           <div class="headline">
             Насколько долго гости могут жить в вашем жилье?
           </div>
-          <div class="d-flex">
-            <div class="d-inline">
-              <div class="d-inline-block">
-                <v-text-field
-                  v-model="nightsMin"
-                  type="number"
-                  min="0"
-                  placeholder="ночей минимум"
-                  @input="minCheckDisable"
-                />
-              </div>
-              <v-btn
-                :disabled="minDisabledButton"
-                rounded
-                @click="nightsMinDecrement"
-                >-</v-btn
-              >
-              <v-btn rounded @click="nightsMinIncrement">+</v-btn>
-            </div>
-          </div>
-          <div class="d-flex">
-            <div class="d-inline">
-              <div class="d-inline-block">
-                <v-text-field
-                  v-model="nightsMax"
-                  type="number"
-                  min="0"
-                  placeholder="ночей максимум"
-                  @input="maxCheckDisable"
-                />
-              </div>
-              <v-btn
-                :disabled="maxDisabledButton"
-                rounded
-                @click="nightsMaxDecrement"
-                >-</v-btn
-              >
-              <v-btn rounded @click="nightsMaxIncrement">+</v-btn>
-            </div>
-          </div>
+          <IncrementDecrement placeholder="Ночей минимум" />
+          <IncrementDecrement placeholder="Ночей максимум" />
         </v-card-text>
       </v-card>
     </v-col>
@@ -53,58 +15,17 @@
 </template>
 
 <script>
+import IncrementDecrement from '~/components/inputs/IncrementDecrement'
 export default {
   name: 'WizardHowLongCanGuestsStay',
+  components: {
+    IncrementDecrement
+  },
   data: () => ({
     nightsMin: '',
-    nightsMax: '',
-    minDisabledButton: null,
-    maxDisabledButton: null
+    nightsMax: ''
   }),
-  methods: {
-    nightsMinIncrement() {
-      this.nightsMin++
-      this.minDisabledButton = false
-    },
-    nightsMinDecrement() {
-      if (this.nightsMin > 0) {
-        this.minDisabledButton = false
-        this.nightsMin--
-      }
-
-      if (this.nightsMin === 0) {
-        this.minDisabledButton = true
-      }
-    },
-    nightsMaxIncrement() {
-      this.nightsMax++
-      this.maxDisabledButton = false
-    },
-    nightsMaxDecrement() {
-      if (this.nightsMax > 0) {
-        this.maxDisabledButton = false
-        this.nightsMax--
-      }
-
-      if (this.nightsMax === 0) {
-        this.maxDisabledButton = true
-      }
-    },
-    minCheckDisable() {
-      if (this.nightsMin <= 0) {
-        this.minDisabledButton = true
-      } else {
-        this.minDisabledButton = false
-      }
-    },
-    maxCheckDisable() {
-      if (this.nightsMin <= 0) {
-        this.maxDisabledButton = true
-      } else {
-        this.maxDisabledButton = false
-      }
-    }
-  }
+  methods: {}
 }
 </script>
 
