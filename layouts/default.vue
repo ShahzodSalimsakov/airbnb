cb308c329e768267527c2183d5628ffb005f2d28
shahaa <projects.salim95@gmail.com>
2020-06-15 11:46:30 +0500

    added header footer component

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,14 +2,31 @@
   <v-app dark>
     <v-content>
       <v-container>
-        <nuxt />
+        <Header />
+        <v-layout column justify-center align-center>
+          <v-flex xs12 sm8 md6>
+            <div class="text-center">
+              <h1>
+                <nuxt-link to="/become-a-host">step 1</nuxt-link>
+                <br />
+                <nuxt-link to="/step2">step 2</nuxt-link>
+                <br />
+                <nuxt-link to="/step3">step 3</nuxt-link>
+              </h1>
+            </div>
+          </v-flex>
+        </v-layout>
+        <Footer />
       </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import Header from '~/layouts/components/header'
+import Footer from '~/layouts/components/footer'
 export default {
+  components: { Header, Footer },
   data() {
     return {}
   }
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,10 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <div class="text-center">
-        <h1>
-          <nuxt-link to="/become-a-host">step 1</nuxt-link>
-          <br />
-          <nuxt-link to="/step2">step 2</nuxt-link>
-          <br />
-          <nuxt-link to="/step3">step 3</nuxt-link>
-        </h1>
-      </div>
-    </v-flex>
-  </v-layout>
+  <nuxt />
 </template>
 
 <script>
 export default {
-  components: {}
+  components: {},
+  layout: ''
 }
 </script>
