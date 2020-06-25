1d3694462b20e8d505ba5ad832635ea66371120a
Dave93 <yul.davron.93@gmail.com>
2020-06-25 11:25:08 +0500

    signin

--- components/auth/login.vue
+++ components/auth/login.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-dialog v-model="dialog" persistent max-width="300px">
+  <v-dialog v-model="dialog" max-width="300px">
     <template v-slot:activator="{ on, attrs }">
       <div
         class="block mt-2 lg:inline-block lg:mt-0 mr-4 text-base"
@@ -10,59 +10,105 @@
       </div>
     </template>
     <div class="w-full max-w-xs">
-      <form class="bg-white shadow-md rounded px-8 pt-6 pb-8">
-        <div class="mb-4">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
-            Email
-          </label>
-          <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="email"
-            type="text"
-            placeholder="Email"
-          />
-        </div>
-        <div class="mb-6">
-          <label
-            class="block text-gray-700 text-sm font-bold mb-2"
-            for="password"
-          >
-            Пароль
-          </label>
-          <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="password"
-            type="password"
-            placeholder="******************"
-          />
-        </div>
-        <div class="flex items-center justify-between">
-          <button
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button"
-            @click="dialog = false"
+      <v-form
+        v-model="regFormValid"
+        class="bg-white shadow-md rounded px-8 pt-6 pb-8"
+      >
+        <span class="block font-semibold mb-4 text-2xl uppercase">Войти</span>
+        <v-alert v-show="!!errors.email" color="red lighten-2" dark>
+          {{ errors.email && errors.email[0] }}
+        </v-alert>
+        <v-row>
+          <v-col cols="12">
+            <v-text-field
+              v-model="form.email"
+              :rules="rules.email"
+              label="Email"
+              placeholder="Email"
+              outlined
+              type="email"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12">
+            <v-text-field
+              v-model="form.password"
+              :rules="rules.password"
+              :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="show ? 'text' : 'password'"
+              label="Пароль"
+              placeholder="******"
+              outlined
+              hint="Не менее 6 символов"
+              counter
+              @click:append="show = !show"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <div class="flex items-center justify-end">
+          <v-btn
+            :disabled="!regFormValid"
+            color="primary"
+            class="mr-4"
+            :loading="formLoading"
+            @click="signin"
           >
             Войти
-          </button>
-          <a
-            class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-            href="#"
-            @click="dialog = false"
-          >
-            Забыли пароль?
-          </a>
+          </v-btn>
         </div>
-      </form>
+      </v-form>
     </div>
   </v-dialog>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
   name: 'login',
   data() {
     return {
+      formLoading: false,
+      regFormValid: false,
+      show: false,
+      rules: {
+        email: [
+          (v) => !!v || 'E-mail обязательно для заполнения',
+          (v) => /.+@.+\..+/.test(v) || 'E-mail указан неверный'
+        ],
+        password: [
+          (v) => (v && v.length >= 6) || 'Пароль должен быть длинее 6 символов'
+        ]
+      },
+      form: {
+        password: '',
+        email: ''
+      },
       dialog: false
+    }
+  },
+  computed: {
+    ...mapGetters({
+      errors: 'validation/errors'
+    })
+  },
+  methods: {
+    async signin() {
+      this.formLoading = true
+      try {
+        await this.$auth.loginWith('local', {
+          data: {
+            email: this.form.email,
+            password: this.form.password
+          }
+        })
+        this.formLoading = false
+        this.dialog = false
+        this.$store.dispatch('validation/clearErrors')
+        await this.$router.push('/')
+      } catch (e) {
+        this.formLoading = false
+      }
     }
   }
 }
